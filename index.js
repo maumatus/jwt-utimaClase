2e45df379e42c1c7a44a70ceb84b16c1218622ed
maumatus <86168992+maumatus@users.noreply.github.com>
2021-11-05 10:10:01 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+
+//Configurar
+app.listen(3000,()=>console.log("Ejecutando por puerto http://localhost:3000"));
+
+//Config handlebars
+app.set("view engine","handlebars");
+
+app.engine(
+    "handlebars",
+    handlebars({
+        layoutsDir: __dirname + "/views",
+        partialsDir: __dirname + "/views/partials"
+    })
+);
+
+//Middleware apra Jquery y Bootstrap
+app.use("/bootstrap", express.static(__dirname + '/node_modules/bootstrap/dist/'))
+app.use("/jquery", express.static(__dirname + '/node_modules/jquery/dist/'))
+
+
+//Publicar ruta base
+app.use("/",(req,res)=>{
+    res.render("Inicio",{
+        layout: "Inicio"
+    })
+});
 
